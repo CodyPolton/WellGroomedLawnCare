5ee36a1ac0e7a2b8d566f62025ee1ca1fae7c33a
Cody Polton <cj.polton@gmail.com>
2019-10-31 10:16:21 -0500

    Made inovice email system

--- EmailInvoice.py
+++ EmailInvoice.py
@@ -6,9 +6,19 @@ from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 
 subject = "Test Email From Cody"
-body = "Let me know if you get this and can see the attachment. Reply back to my normal email. Do not reply to this email, this is an automated message."
+body = """\
+<html>
+  <body>
+    <p>Hi,<br>
+       How are you?<br>
+       <a href="http://www.realpython.com">Real Python</a> 
+       has many great tutorials.
+    </p>
+  </body>
+</html>
+"""
 sender_email = "testEmailWellGroomed@gmail.com"
-receiver_email = "ggwis@msn.com"
+receiver_email = "cj.polton@gmail.com"
 password = "Groomed1!"
 
 # Create a multipart message and set headers
@@ -19,9 +29,9 @@ message["Subject"] = subject
 message["Bcc"] = receiver_email  # Recommended for mass emails
 
 # Add body to email
-message.attach(MIMEText(body, "plain"))
+message.attach(MIMEText(body, "html"))
 
-filename = "test-output.docx"  # In same directory as script
+filename = "Invoices/test-output.docx"  # In same directory as script
 
 # Open PDF file in binary mode
 with open(filename, "rb") as attachment:
@@ -36,7 +46,7 @@ encoders.encode_base64(part)
 # Add header as key/value pair to attachment part
 part.add_header(
     "Content-Disposition",
-    f"attachment; filename= {filename}",
+    "attachment; filename={}".format(filename),
 )
 
 # Add attachment to message and convert message to string
@@ -45,6 +55,7 @@ text = message.as_string()
 
 # Log in to server using secure context and send email
 context = ssl.create_default_context()
-with smtplib.SMTP_SSL("smtp.gmail.com", 465, context=context) as server:
-    server.login(sender_email, password)
-    server.sendmail(sender_email, receiver_email, text)
+server = smtplib.SMTP_SSL("smtp.gmail.com", 465)
+server.login(sender_email, password)
+server.sendmail(sender_email, receiver_email, text)
+server.quit()
